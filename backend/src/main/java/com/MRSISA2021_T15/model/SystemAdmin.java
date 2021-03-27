7893863136fdbd1f04bfbb7bab7b70e8da32300b
ivancolak <ivancolaksc2@gmail.com>
2021-03-27 16:30:02 +0100

    database creation

--- backend/src/main/java/com/MRSISA2021_T15/model/SystemAdmin.java
+++ backend/src/main/java/com/MRSISA2021_T15/model/SystemAdmin.java
@@ -1,12 +1,19 @@
 package com.MRSISA2021_T15.model;
 
-public class SystemAdmin extends User{
+import javax.persistence.*;
 
+@Entity
+@Table(name = "system_admin")
+public class SystemAdmin extends User{
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Integer dbId;
 	public SystemAdmin() {
 		super();
 	}
 
-	public SystemAdmin(int id, String email, String name, String surname, String adress, String city, String country,
+	public SystemAdmin(
+			int id, String email, String name, String surname, String adress, String city, String country,
 			String phoneNumber, String username, String password) {
 		super(id, email, name, surname, adress, city, country, phoneNumber, username, password);
 	}
