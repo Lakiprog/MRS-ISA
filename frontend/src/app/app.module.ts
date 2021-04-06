e642b322d8f8fff7129faa7c157a8b7466086fb2
Tacaa <t.aca220799@hotmail.com>
2021-04-06 20:45:34 +0200

    Patients profile done and change username and password done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +10,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular_material.module';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UserHomePageComponent } from './user-home-page/user-home-page.component'
+import { UserHomePageComponent } from './user-home-page/user-home-page.component';
 import { PharmacistHomePageComponent } from './pharmacist-home-page/pharmacist-home-page.component';
 import { DermatologistHomePageComponent } from './dermatologist-home-page/dermatologist-home-page.component';
 import { PharmacistPatientComponent } from './pharmacist-patients/pharmacist-patient.component';
 import { DermatologistPatientComponent } from './dermatologist-patients/dermatologist-patient.component';
+import { PatientProfileNavbarComponent } from './patient-profile-navbar/patient-profile-navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ChangePatientDataComponent } from './change-patient-data/change-patient-data.component';
+
+
+
 
 
 @NgModule({
@@ -25,7 +37,9 @@ import { DermatologistPatientComponent } from './dermatologist-patients/dermatol
     PharmacistHomePageComponent,
     DermatologistHomePageComponent,
     PharmacistPatientComponent,
-    DermatologistPatientComponent
+    DermatologistPatientComponent,
+    PatientProfileNavbarComponent,
+    ChangePatientDataComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +47,7 @@ import { DermatologistPatientComponent } from './dermatologist-patients/dermatol
     BrowserAnimationsModule,
     AngularMaterialModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'register', component: RegistrationComponent },
@@ -41,8 +56,18 @@ import { DermatologistPatientComponent } from './dermatologist-patients/dermatol
       { path: 'PharmacistHomePage', component: PharmacistHomePageComponent },
       { path: 'DermatologistHomePage', component: DermatologistHomePageComponent },
       { path: 'PharmacistPatientComponent', component: PharmacistPatientComponent},
-      { path: 'DermatologistPatientComponent', component: DermatologistPatientComponent}
-    ])
+      { path: 'DermatologistPatientComponent', component: DermatologistPatientComponent},
+      { path: 'PatientProfile', component:PatientProfileNavbarComponent},
+      { path: 'PatientChangeData', component:ChangePatientDataComponent}
+    ]),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
